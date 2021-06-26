e32222bdd66fb9beef064895756a3b33e8e7fe16
Francisco <oficialkikolms123@gmail.com>
2021-06-26 11:39:39 +0100

    Third Day of NLWTogether

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
 
 import { router } from "./routes";
 
@@ -11,7 +12,20 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
+//Midleware of exceptions
+app.use((err: Error, request: Request, response: Response, next: NextFunction) =>{
+    if(err instanceof Error){//Send the erros that we handled
+        return response.status(400).json({
+            error: err.message
+        });
+    }
 
+    //Send the erros that i dont have handled
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    });
+});
 
 
 app.listen(PORT, () => {
